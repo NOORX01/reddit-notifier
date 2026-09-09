f7743569001c56871c69cd32a9a3d407c5e7f553
NOORX01 <noor.secretmail@gmail.com>
2026-09-09 11:47:52 +0200

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -26,7 +26,7 @@ SUBREDDITS = [
 ]
 
 KEYWORDS = [
-    "profits", "money", "eval", "strat", "strategy", "prop firm", "funded account",
+    "Depuis", "profits", "money", "eval", "strat", "strategy", "prop firm", "funded account",
     "evaluation fee", "challenge fee", "FTMO", "MyForexFunds", "prop firm scam",
     "failed evaluation", "passed evaluation", "payout rules", "drawdown rules",
     "profit split", "need capital", "trading capital", "get funded",
